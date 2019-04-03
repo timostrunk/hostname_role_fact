906610c2799f20be40710d931e1540a58f760a33
Simon Kaiser <simon.kaiser@kit.edu>
2019-04-03 15:45:37 +0200

    Parse hostnames containing hyphens

--- lib/facter/hostname_role_fact.rb
+++ lib/facter/hostname_role_fact.rb
@@ -1,13 +1,13 @@
-# ([a-z]+)[0-9]+, i.e. www01 or logger22 have a hostname_role of www or logger
-if Facter.value(:hostname) =~ /^([a-z]+)[0-9]+$/
+# ([a-z-]+?)-?[0-9]+, i.e. www01 or logger-dev-22 have a hostname_role of www or logger-dev
+if Facter.value(:hostname) =~ /^([a-z-]+?)-?[0-9]+$/
   Facter.add('hostname_role') do
     setcode do
       $1
     end
   end
 
-# ([a-z]+), i.e. www or logger have a hostname_role of www or logger
-elsif Facter.value(:hostname) =~ /^([a-z]+)$/
+# ([a-z-]+), i.e. www or logger-dev have a hostname_role of www or logger-dev
+elsif Facter.value(:hostname) =~ /^([a-z-]+)$/
   Facter.add('hostname_role') do
     setcode do
       $1
